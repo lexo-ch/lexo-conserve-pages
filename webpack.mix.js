6ae03b9415b8e4774efa3fa1073a8b0251cabbe5
Miljan Puzović <mpuzovic@lexo.ch>
2025-01-02 17:04:07 +0100

    fix(deps): Update dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,22 +1,15 @@
 const mix = require('laravel-mix');
 
 mix
-  .setPublicPath('dist')
-  .browserSync({
-    proxy: "http://test.test",
-    files: [
-      'dist/**/**',
-      '**/*.php',
-    ],
-  });
+    .setPublicPath('dist');
 
 mix
-  .js('resources/scripts/admin-cp.js', 'js')
-  .sass('resources/styles/admin-cp.scss', 'css')
-  .options({
-    processCssUrls: false,
-  });
+    .js('resources/scripts/admin-cp.js', 'js')
+    .sass('resources/styles/admin-cp.scss', 'css')
+    .options({
+        processCssUrls: false,
+    });
 
 mix
-  .version()
-  .sourceMaps();
+    .version()
+    .sourceMaps();
